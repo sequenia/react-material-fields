c902930bfc54438b98b9c38f79e4bae1f35005d6
Semen Merkulov <semen.merkulov@sequenia.com>
2020-09-04 18:36:37 +0700

    Updates

---
+++ precache-manifest.8f3db6fd55edef08aefa440a9eeec3d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "929b30044eb12d7dcd443bf2fb34822b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8563e59744fe6c9429be",
+    "url": "./static/css/main.7cab3132.chunk.css"
+  },
+  {
+    "revision": "e0fb9c83fcba92bc602f",
+    "url": "./static/js/2.6e128cc9.chunk.js"
+  },
+  {
+    "revision": "556d72da1c19c1e88e2917fa6505d7f0",
+    "url": "./static/js/2.6e128cc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8563e59744fe6c9429be",
+    "url": "./static/js/main.826ff54d.chunk.js"
+  },
+  {
+    "revision": "ec42ee92bf145591b40d",
+    "url": "./static/js/runtime-main.e72a8176.js"
+  }
+]);
